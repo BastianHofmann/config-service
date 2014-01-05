d5a46935d30daedf852120f6068cf28c189260de
Bastian Hofmann <bastian@Bastians-MacBook-Pro.local>
2014-01-05 16:03:33 +0100

    Remove testing files

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$config = new Zilla\Config\ConfigService([], new Zilla\Config\PHPStore(__DIR__));
-
-var_dump($config->get('foo.bar'));
-
-var_dump($config->get('config.some.bar'));
